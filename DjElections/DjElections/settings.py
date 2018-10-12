1e3cb627347d2fb96f2d7034f24bbbac59f65be0
DESKTOP-A15HHS9\Leonid <leonidvishniakov@gmail.com>
2018-10-12 18:26:31 +0300

    second commit

--- DjElections/DjElections/settings.py
+++ DjElections/DjElections/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'DjApp.apps.DjappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

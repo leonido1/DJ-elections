8dd91459d21eaa1eb3c4d91361a337d313e01044
DESKTOP-A15HHS9\Leonid <leonidvishniakov@gmail.com>
2018-10-13 11:31:07 +0300

    Third commit

--- DjElections/DjApp/admin.py
+++ DjElections/DjApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.DJ)
+admin.site.register(models.Song)
+admin.site.register(models.Elections)
+admin.site.register(models.SongsInElections)
--- DjElections/DjApp/models.py
+++ DjElections/DjApp/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+
+class DJ (User):
+    pass
+
+
+class Song (models.Model):
+    dj =  models.ManyToManyField(DJ)
+    name = models.TextField()
+    artist = models.TextField()
+
+
+class Elections(models.Model):
+    dj = models.ForeignKey(DJ, on_delete = models.DO_NOTHING)
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+
+class SongsInElections(models.Model):
+    elections = models.ForeignKey(Elections, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    votes =  models.IntegerField()
+
+
+
+
